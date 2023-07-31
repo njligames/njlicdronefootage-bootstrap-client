86221a9b2084a27b256bc86cbfa9c588bcd32f9e
James Folk <jamesfolk1@gmail.com>
2023-07-30 20:30:29 -0400

    updated contact page

--- assets/js/main.js
+++ assets/js/main.js
@@ -287,10 +287,10 @@
 
   //17.  Progress barfiller
 
-  $("#bar1").barfiller();
-  $("#bar2").barfiller();
-  $("#bar3").barfiller();
-  $("#bar4").barfiller();
-  $("#bar5").barfiller();
-  $("#bar6").barfiller();
+  // $("#bar1").barfiller();
+  // $("#bar2").barfiller();
+  // $("#bar3").barfiller();
+  // $("#bar4").barfiller();
+  // $("#bar5").barfiller();
+  // $("#bar6").barfiller();
 })(jQuery);
